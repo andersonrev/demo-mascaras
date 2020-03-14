c53dc2f3938727ed707aa07ce9e2c224e790f872
andersonrev <anderson.revelo@epn.edu.ec>
2020-03-14 14:56:48 -0400

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  title = 'Máscaras ';
+  phone: number;
+  precioAuto: number = 0;
+  peso: number = 0;
+
+
+  phoneMask = ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+
+  placaAuto = {
+    mask: [/[A-Z]/, /[A-Z]/, /[A-Z]/, '-', /\d/, /\d/, /\d/, /\d/],
+    guide: false
+  };
+
+  cedulaMask = {
+    mask: [/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/],
+    guide: false
+
+  };
+
+  configuracionMascaraPeso = {
+    prefix: '',
+    suffix: 'Kg',
+    align: 'left',
+    thousands: '.',
+    decimal: ',',
+    precision: 4
+  };
+
+
+  constructor() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import {CurrencyMaskModule} from 'ng2-currency-mask';
+import {TextMaskModule} from 'angular2-text-mask'
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+   TextMaskModule,
+    CurrencyMaskModule, ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
